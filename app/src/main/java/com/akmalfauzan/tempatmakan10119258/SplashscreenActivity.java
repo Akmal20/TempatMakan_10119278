e7062cb0065e3c5e99ccf7c3b643f2b9c434a1a6
akmal20 <Akmalfauzan4321@gmail.com>
2022-07-01 23:09:59 +0700

    Initial commit

---
+++ app/src/main/java/com/akmalfauzan/tempatmakan10119258/SplashscreenActivity.java
@@ -0,0 +1,30 @@
+package com.akmalfauzan.tempatmakan10119258;
+/*
+    nim                 : 10119258
+    nama                : Akmal Jihad Fauzan
+    kelas               : IF-7
+*/
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
